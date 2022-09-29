98939b1e5b497ec23158eec4cd236206bb6e12ab
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-09-29 21:46:34 +0700

    update
    update

---
+++ includes/header.inc.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>iDoor</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/animate.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">iDoor</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                </li> -->
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php require "./includes/header.inc.php" ?>
+
+<div class="hero">
+    <div id="stage">
+        <div id="rotate">
+            <div id="ring-1" class="ring"></div>
+            <div id="ring-2" class="ring"></div>
+            <div id="ring-3" class="ring"></div>
+            <div id="ring-4" class="ring"></div>
+            <div id="ring-5" class="ring"></div>
+        </div>
+    </div>
+
+    <div class="line-color"></div>
+    
+    <div class="item-container">
+        <div class="wow flash">
+            <img src="assets/images/door1.png" alt="" class="door">
+        </div>
+        <div class="wow bounceInDown">
+            <img src="assets/images/door2.png" alt="" class="door">
+        </div>
+        <div class="wow shake">
+            <img src="assets/images/door3.png" alt="" class="door">
+        </div>
+    </div>
+    <div class="item-container">
+        <div class="wow flipInX">
+            <img src="assets/images/door4.png" alt="" class="door">
+        </div>
+        <div class="wow flipInY">
+            <img src="assets/images/door5.png" alt="" class="door">
+        </div>
+        <div class="wow flipInX">
+            <img src="assets/images/door6.png" alt="" class="door">
+        </div>
+    </div>
+    <div class="item-container">
+        <div class="wow lightSpeedIn">
+            <img src="assets/images/door7.png" alt="" class="door">
+            <div class="item-desc">
+                <div>Item Name</div>
+                <div>Desc</div>
+            </div>
+        </div>
+        <div class="wow tada">
+            <img src="assets/images/door8.png" alt="" class="door">
+            <div class="item-desc">
+                <div>Item Name</div>
+                <div>Desc</div>
+            </div>
+        </div>
+        <div class="wow pulse">
+            <img src="assets/images/door9.png" alt="" class="door">
+            <div class="item-desc">
+                <div>Item Name</div>
+                <div>Desc</div>
+            </div>
+        </div>
+    </div>
+        
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require "./includes/footer.inc.php" ?>
