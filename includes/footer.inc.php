79e8a88495200af275321be8491d78673de38eb2
Excel Antonio <ordon27e@gmal.com>
2022-10-03 14:58:46 +0700

    add slider and detail page

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,6 +1,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 <script type="text/javascript">
     const POSTERS_PER_ROW = 12;
     const RING_RADIUS = 200;
@@ -37,7 +38,22 @@
 <script src="../assets/js/wow.min.js"></script>
 <script>
     
-    new WOW().init();
+    // new WOW().init();
+    const swiper = new Swiper('.swiper', {
+  // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+    });
 </script>
 </body>
 
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php require "./includes/header.inc.php" ?>
-
+<?php
+    $q = "SELECT * FROM doors";
+    $result = $conn->query($q);
+    
+   
+?>
 <div class="hero">
     <h1 id="slogan">We Sell Futuristic Doors You can Never Imagine</h1>
     <div id="stage">
@@ -19,49 +24,92 @@
 <h1 class="titleshop">Door Selection</h1>
 
 <div class="item-container">
-    <div class="wow flash">
+    
+</div>
+<div class="item-container">
+    
+</div>
+<div class="item-container">
+    
+</div>
+
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <?php
+        if ($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                // print_r(($row["door_id"]));
+    
+    ?>
+        <div class="swiper-slide">
+            <img src=<?php echo "assets/images/door".$row["door_id"].".png" ?> alt="" class="door">
+            <div class="door-desc-text">
+                <h2>
+                    <?=$row["door_name"]?>
+                </h2>
+                <p>
+                    <?=$row["door_desc"]?>
+                </p>
+                <p>
+                    <?="Rp.".$row["door_price"]?>
+                </p>
+                <a href=<?= "door_detail.php?id=".$row["door_id"]?>>
+                    <button>Detail</button>
+                </a>
+            </div>
+        </div>
+    <?php
+        }
+    }
+    ?>
+    <!-- <div class="swiper-slide">
         <img src="assets/images/door1.png" alt="" class="door">
     </div>
-    <div class="wow bounceInDown">
+    <div class="swiper-slide">
         <img src="assets/images/door2.png" alt="" class="door">
     </div>
-    <div class="wow shake">
+    <div class="swiper-slide">
         <img src="assets/images/door3.png" alt="" class="door">
     </div>
-</div>
-<div class="item-container">
-    <div class="wow flipInX">
+    <div class="swiper-slide">
         <img src="assets/images/door4.png" alt="" class="door">
     </div>
-    <div class="wow flipInY">
+    <div class="swiper-slide">
         <img src="assets/images/door5.png" alt="" class="door">
     </div>
-    <div class="wow flipInX">
+    <div class="swiper-slide">
         <img src="assets/images/door6.png" alt="" class="door">
     </div>
-</div>
-<div class="item-container">
-    <div class="wow lightSpeedIn">
+    <div class="swiper-slide">
         <img src="assets/images/door7.png" alt="" class="door">
         <div class="item-desc">
             <div>Item Name</div>
             <div>Desc</div>
         </div>
     </div>
-    <div class="wow tada">
+    <div class="swiper-slide">
         <img src="assets/images/door8.png" alt="" class="door">
         <div class="item-desc">
             <div>Item Name</div>
             <div>Desc</div>
         </div>
     </div>
-    <div class="wow pulse">
+    <div class="swiper-slide">
         <img src="assets/images/door9.png" alt="" class="door">
         <div class="item-desc">
             <div>Item Name</div>
             <div>Desc</div>
         </div>
     </div>
+    ... -->
+  </div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
 </div>
 
 
